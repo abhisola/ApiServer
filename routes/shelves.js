13817c2a3f93125b840cd4a3b284404882ad87e3
abhisola <abhishek.solapurkar@itconnectus.com>
2018-06-23 15:37:26 +0530

    Fixed Report Show And Report Send

--- routes/shelves.js
+++ routes/shelves.js
@@ -49,109 +49,193 @@ router
         })
 })
 .post('/api/range/:_num', function(req, res, next) {
-      var data = req.body;
-      var racknum = req.params['_num'];
-      var start = data.startDate;
-      var end = data.endDate;
+  var data = req.body;
+  var racknum = req.params['_num'];
+  var start = data.startDate;
+  var end = data.endDate;
 
-      var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
-        "WHERE date_recorded > '" + start + "' AND  date_recorded < '" + end + "' " +
-              "AND racknum = '"+racknum+"' " +
-              "ORDER BY date_recorded ASC";
-      console.log(querry);
-      var client = new Client(settings.database.postgres);
-      client.connect();
+  var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
+    "WHERE date_recorded > '" + start + "' AND  date_recorded < '" + end + "' " +
+          "AND racknum = '"+racknum+"' " +
+          "ORDER BY date_recorded ASC";
+  console.log(querry);
+  var client = new Client(settings.database.postgres);
+  client.connect();
 
-      client.query(querry, function (err, dbres){
-        console.log(err, dbres);
-        if(dbres) {
-          var output = _.groupBy(dbres.rows, function(b) { return b.shelf_num;});
-          res.json({err:null,data:output});
-        } else {
-          res.json({err:dbres,data:[]});
-        }
-        client.end();
-      });
+  client.query(querry, function (err, dbres){
+    console.log(err, dbres);
+    if(dbres) {
+      var output = _.groupBy(dbres.rows, function(b) { return b.shelf_num;});
+      res.json({err:null,data:output});
+    } else {
+      res.json({err:dbres,data:[]});
+    }
+    client.end();
+  });
 })
-
-.get('/api/sendreport/yesterday/:_num', function(req, res, next){
-      var data = req.body;
-      var racknum = req.params['_num'];
-      var start = getDate() + "T00:01:00";
-      var end = getDate() + "T23:59:00";
-      var fetchRack = "SELECT * from racks WHERE racknum='"+racknum+"'";
-      var client = new Client(settings.database.postgres);
-      client.connect();
-      client.query(fetchRack, function(err, dbres) {
-        if(dbres) {
-          if(dbres.rowCount > 0) {
-            var rack = dbres.rows[0];
-            console.log(rack);
-            var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
-              "WHERE date_recorded > '" + start + "' AND  date_recorded < '" + end + "' " +
-              "AND racknum = '" + racknum + "' " +
-              "ORDER BY date_recorded DESC , shelf_num ASC LIMIT "+rack.shelves;
-              console.log(querry);
-            client.query(querry, function(err, dbresponse){
-              if (dbresponse) {
-                var format_start = DateTime.fromISO(start).toFormat('LLL dd, HH:mma');
-                var format_end = DateTime.fromISO(end).toFormat('LLL dd, HH:mma');
-                var template_data = {
-                  rackname : rack.name,
-                  address : rack.address,
-                  start: format_start,
-                  end: format_end,
-                  shelves : []
-                }
-                _.forEach(dbresponse.rows, function (row,i) {
-                  if(i == row.shelf_num) {
-                    var shelf = {
-                      percent: Math.round(100 - parseInt(row.percent)),
-                      shelf_num: row.shelf_num,
-                      url: row.url
-                    }
-                    template_data.shelves.push(shelf);
+.get('/api/sendreport/yesterday/:_num', function (req, res, next){
+    var racknum = req.params['_num'];
+  res.redirect('/shelves/api/sendreport/0/' + racknum);
+})
+.get('/api/sendreport/:_days/:_num', function(req, res, next){
+    var data = req.body;
+    var racknum = req.params['_num'];
+    var days = parseInt(req.params['_days']);
+    days = days + 1;
+    var start = getDate(days) + "T00:01:00";
+    var end = getDate(days) + "T23:59:00";
+    var fetchRack = "SELECT * from racks WHERE racknum='"+racknum+"'";
+    var client = new Client(settings.database.postgres);
+    var clientB = new Client(settings.database.postgres);
+    client.connect();
+    clientB.connect();
+    client.query(fetchRack, function(err, dbres) {
+      if(dbres) {
+        if(dbres.rowCount > 0) {
+          var rack = dbres.rows[0];
+          var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
+            "WHERE date_recorded > '" + start + "' AND  date_recorded < '" + end + "' " +
+            "AND racknum = '" + racknum + "' " +
+            "ORDER BY date_recorded DESC , shelf_num ASC LIMIT "+rack.shelves;
+            console.log(querry);
+          clientB.query(querry, function (err, dbresponse) {
+            if (dbresponse) {
+              var format_start = DateTime.fromISO(start).toFormat('LLL dd, HH:mma');
+              var format_end = DateTime.fromISO(end).toFormat('LLL dd, HH:mma');
+              var template_data = {
+                rackname : rack.name,
+                address : rack.address,
+                start: format_start,
+                end: format_end,
+                shelves : []
+              }
+              _.forEach(dbresponse.rows, function (row, i) {
+                if(i == row.shelf_num) {
+                  var shelf = {
+                    percent: Math.round(100 - parseInt(row.percent)),
+                    shelf_num: row.shelf_num,
+                    url: row.url
                   }
-                })
-                mailer.send({
-                  from: sent_from,
-                  to: sent_to, //List of recievers,
-                  subject: 'Target Smart Shelf - Out of Stock Alerts from Kellogs',
-                  template: 'shelf',
-                  data: template_data
-                })
-                .then(resp=> {
-                  res.render('../mails/templates/shelf', template_data);
-                })
-                .catch(err=>{
-                  res.json({
-                    success: false,
-                    msg: 'Error Sending Mail!',
-                    data: []
-                  });
-                })
-              } else {
-                res.json({ success: false, msg: 'Something Went Wrong While fetching data', data: [] });
+                  template_data.shelves.push(shelf);
+                }
+              })
+              console.log(template_data);
+              mailer.send({
+                from: sent_from,
+                to: sent_to, //List of recievers,
+                subject: 'Target Smart Shelf - Out of Stock Alerts from Kellogs',
+                template: 'shelf',
+                data: template_data
+              })
+              .then(resp=> {
+                res.render('../mails/templates/shelf', template_data);
+              })
+              .catch(err=>{
+                res.json({
+                  success: false,
+                  msg: 'Error Sending Mail!',
+                  data: []
+                });
+              })
+            } else {
+              console.log('Error: '+JSON.stringify(err));
+              console.log('DBRES: ' + JSON.stringify(dbres));
+              res.json({ success: false, msg: 'No Data Found for range : '+start+' To '+end, data: [] });
+            }
+            clientB.end();
+          })
+
+        } else {
+            res.json({
+              success: false,
+              msg: 'Rack Not Found',
+              data: []
+            });
+        }
+      } else {
+        res.json({success: false, msg: 'Something Went Wrong', data: []});
+      }
+      client.end();
+    });
+    
+})
+.get('/api/showreport/:_days/:_num', function (req, res, next) {
+  var data = req.body;
+  var racknum = req.params['_num'];
+  var days = parseInt(req.params['_days']);
+  days = days + 1;
+  var start = getDate(days) + "T00:01:00";
+  var end = getDate(days) + "T23:59:00";
+  var fetchRack = "SELECT * from racks WHERE racknum='" + racknum + "'";
+  var client = new Client(settings.database.postgres);
+  var clientB = new Client(settings.database.postgres);
+  client.connect();
+  clientB.connect();
+  client.query(fetchRack, function (err, dbres) {
+    if (dbres) {
+      if (dbres.rowCount > 0) {
+        var rack = dbres.rows[0];
+        var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
+          "WHERE date_recorded > '" + start + "' AND  date_recorded < '" + end + "' " +
+          "AND racknum = '" + racknum + "' " +
+          "ORDER BY date_recorded DESC , shelf_num ASC LIMIT " + rack.shelves;
+        console.log(querry);
+        clientB.query(querry, function (err, dbresponse) {
+          if (dbresponse) {
+            var format_start = DateTime.fromISO(start).toFormat('LLL dd, HH:mma');
+            var format_end = DateTime.fromISO(end).toFormat('LLL dd, HH:mma');
+            var template_data = {
+              rackname: rack.name,
+              address: rack.address,
+              start: format_start,
+              end: format_end,
+              shelves: []
+            }
+            _.forEach(dbresponse.rows, function (row, i) {
+              if (i == row.shelf_num) {
+                var shelf = {
+                  percent: Math.round(100 - parseInt(row.percent)),
+                  shelf_num: row.shelf_num,
+                  url: row.url
+                }
+                template_data.shelves.push(shelf);
               }
             })
-
+            console.log(template_data);
+            res.render('../mails/templates/shelf', template_data);
           } else {
-             res.json({
-               success: false,
-               msg: 'Rack Not Found',
-               data: []
-             });
+            console.log('Error: ' + JSON.stringify(err));
+            console.log('DBRES: ' + JSON.stringify(dbres));
+            res.json({
+              success: false,
+              msg: 'No Data Found for range : ' + start + ' To ' + end,
+              data: []
+            });
           }
-        } else {
-          res.json({success: false, msg: 'Something Went Wrong', data: []});
-        }
-        client.end();
+          clientB.end();
+        })
+
+      } else {
+        res.json({
+          success: false,
+          msg: 'Rack Not Found',
+          data: []
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        msg: 'Something Went Wrong',
+        data: []
       });
-      
+    }
+    client.end();
+  });
+
 })
 ;
 
-function getDate() {
+function getDate(days) {
   var today = new Date();
 
   var year = today.getFullYear();
@@ -160,7 +244,7 @@ function getDate() {
   month = (month < 10 ? "0" : '') + month;
 
   var day = today.getDate();
-  day = day-2;
+  day = day - days;
   day = (day < 10 ? "0" : '') + day;
 
   return year + "-" + month + "-" + day;
